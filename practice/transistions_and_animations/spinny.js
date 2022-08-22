e8c00f0f506a53de3197d8a0f4176212a70bd37f
Tom <tom.slaytor@gmail.com>
2022-08-22 21:43:55 +0300

    added new interactive squares

--- practice/transistions_and_animations/spinny.js
+++ practice/transistions_and_animations/spinny.js
@@ -1,6 +1,9 @@
 var grow = document.querySelector('.grow')
 var shrink = document.querySelector('.shrink')
 var colorClick = document.querySelector('.color-click')
+var border = document.querySelector('.border')
+var dashedBorder = document.querySelector('.dashed-border')
+var solidBorder = document.querySelector('.solid-border')
 
 grow.addEventListener('click', function(){
     if (grow.classList.contains('bigger')){
@@ -31,4 +34,26 @@ colorClick.addEventListener('click', function(){
     }
 })
 
+border.addEventListener('click', function(){
+    if (border.classList.contains('border-change')){
+        border.classList.remove('border-change')
+    }
+    else {
 
+        border.classList.add('border-change')
+    }
+})
+
+var dashed = false;
+dashedBorder.addEventListener('click', function(){
+    if (!dashed){
+        dashedBorder.classList.add('visible-dash')
+        solidBorder.classList.add('invisible-border')
+        dashed = true;
+    }
+    else {
+        dashedBorder.classList.remove('visible-dash')
+        solidBorder.classList.remove('invisible-border')
+        dashed = false;
+    }
+})
